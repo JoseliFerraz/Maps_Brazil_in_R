29ac149b76b0c49da721085c2b2fca6fef498d4a
JoseliFerraz <joseli_ferraz@hotmail.com>
2019-08-26 22:00:38 -0300

    Update Maps_Brazil.R

--- Maps_Brazil.R
+++ Maps_Brazil.R
@@ -25,23 +25,15 @@ mapa_emprego+
   labs(title = "Número de empregos formais nas OSCs por região, Brasil - 2015")
 
 
-# Documentação : https://cran.r-project.org/web/packages/brazilmaps/brazilmaps.pdf
-# --------------------------------------------------------------------- # 
-# 2 - MAPA Macroregião
+# Latitude e Longitude das Regiões do Brasil
+Latitude<- c(-60.11427458,-55.11427458,-40.11427458,-52.11427458,-45.11427458)
+Lontgitude<- c(-2.19830591,-14.09999000,-5.99999000,-26.99999000,-20.99999000)
 
-library(tidyverse);library(ggplot2)
-
-map_emprego2<- get_brmap("Region") %>% 
-  inner_join(Empregos_Formais, c("Region" = "Regiao"))
-
-glimpse(map_2)
-
-mapa2<-map_emprego2%>% 
-  ggplot() +
-  geom_sf(aes(fill = Empregos))+
-  # muda escala de cores
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+mapa_emprego+
+  # Adicionando titulo ao grafico
   labs(title = "Número de empregos formais nas OSCs por região, Brasil - 2015")+
-  theme(aspect.ratio=1)# guides(fill=FALSE)
-
-mapa2+guides(fill = guide_legend(label.position = "left", label.hjust = 1))
+  scale_fill_continuous(low = "gray", high = "dark green", 
+                        name = "Quantidade de Empregos (2015)", label = scales::comma, 
+                        breaks=c(70645,670000,1351812))+
+  geom_text(aes(Latitude,Lontgitude,label = Empregos_Formais$Macroregiao),
+            data = Empregos_Formais,  size = 3)
